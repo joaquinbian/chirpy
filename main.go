1180ecf2296341b643df31246a57586050515437
joaquinbian <joacobian21@gmail.com>
2025-12-10 09:18:46 -0300

    metrics

--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareIncHits(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 
@@ -14,14 +26,31 @@ func main() {
 		Addr:        ":8080",
 		IdleTimeout: 10 * time.Second,
 	}
-
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	var cfg = &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	mux.Handle("/app/", cfg.middlewareIncHits(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte("OK"))
 	})
 
+	mux.HandleFunc("/metrics", cfg.handleCountRequests)
+	mux.HandleFunc("/reset", cfg.handleResetCount)
+
 	fmt.Println("server up and listening in 8080!")
 	server.ListenAndServe()
+}
+
+func (cfg *apiConfig) handleCountRequests(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handleResetCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte(fmt.Sprint("count reseted")))
+
 }
